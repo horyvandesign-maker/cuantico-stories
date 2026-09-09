293f9c91681ddd9fb12abffa0f71772b9f6595e8
horyvandesign-maker <horyvan.design@gmail.com>
2026-09-09 12:04:44 -0300

    Update publish_story.py

--- publish_story.py
+++ publish_story.py
@@ -157,11 +157,11 @@ def create_story_template(product, img_obj):
     if product["is_on_demand"]:
         accent_color = ON_DEMAND_COLOR
         display_label = ">> PRODUCTO POR ENCARGUE <<"
-        cta_text = "-> Respondé 'QUIERO' por DM o buscalo en cuanticopc.com.ar <-"
+        cta_text = "Respondé 'QUIERO' por DM o buscalo en cuanticopc.com.ar"
     else:
         accent_color = random.choice(ACCENT_COLORS)
         display_label = product["price"]
-        cta_text = "🔗 Link a la tienda en la Bio | cuanticopc.com.ar"
+        cta_text = "Link a la tienda en la Bio | cuanticopc.com.ar"
     
     bg = img_obj.resize((canvas_w, canvas_h)).filter(ImageFilter.GaussianBlur(50))
     overlay = Image.new("RGBA", (canvas_w, canvas_h), (12, 12, 18, 160))
